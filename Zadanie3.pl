8367b6205cd591987099377a8c6d4bc68b28348f
szymonbanach <banachszymon9@gmail.com>
2025-01-21 19:27:33 +0100

    Add files via upload

---
+++ Zadanie3.pl
@@ -0,0 +1,36 @@
+%Predykaty muszą być dynamiczne by zmieniać ich własności
+:- dynamic produkt/3.
+
+% Fakty o produktach
+produkt(tv, 20, sektor_a).
+produkt(smartfon, 54, sektor_a).
+produkt(amplituner, 30, sektor_b).
+produkt(gramofon, 15, sektor_b).
+produkt(sluchawki, 40, sektor_c).
+produkt(gitara, 12, sektor_c).
+
+% Reguła dostępności produktu
+dostepny_produkt(X) :-
+    produkt(X, N, _),
+    N > 0, % Jeśli ilość(N) jest większa niż 0, program zwróci wartość True
+	write('Status produktu: Dostepny').
+  % Przykładowe zapytanie: dostepny_produkt(tv).
+
+% Reguła przenosząca produkt do innego sektoru
+przenies_produkt(X, Y) :-
+    produkt(X, N, OldY),
+    retract(produkt(X, N, OldY)), % usuwa
+    assertz(produkt(X, N, Y)), % dodaje
+	write(produkt(X, N, Y)). % Nowa lokalizacja produktu
+  % Przykładowe zapytanie: przenies_produkt(tv, sektor_b).
+
+% Reguła dodającą ilość
+uzupelnij_stan(X, N) :-
+    produkt(X, N1, Y),
+    N2 is N1 + N, % Nowa ilość to suma ilości początkowej i ilości podawanej(N)
+    retract(produkt(X, N1, Y)), % usuwa
+	assertz(produkt(X, N2, Y)), % dodaje
+	write(produkt(X, N2, Y)). %Nowa ilość produktu
+  % Przykładowe zapytanie: uzupelnij_stan(tv, 10).
+
+% P.S. Program nie działa w środowisku SWISH, dlatego trzeba go uruchamiać w SWI, dlatego na GitHub załączam również plik z rozszerzeniem pl.
